b70b396e687c9f644527ee89c5f15a68d045fdd1
Hanna <hanna.horbova@gmail.com>
2023-12-26 22:10:37 +0200

    feat: create button and primary styles for it

--- src/layout/Button/Button.tsx
+++ src/layout/Button/Button.tsx
@@ -1,5 +1,39 @@
-const Button = () => {
-	return <div>Button</div>;
+import styles from './Button.module.scss';
+import * as React from 'react';
+
+
+interface IButtonProps {
+	name?: string;
+	btnClasses?: string;
+	onClick?: () => void;
+	type?: 'button' | 'submit' | 'reset';
+	disabled?: boolean;
+	styleBtn?: React.CSSProperties;
+	children?: React.ReactNode;
+}
+
+const Button: React.FC<IButtonProps> = ({
+											name,
+											btnClasses,
+											onClick,
+											type,
+											disabled,
+											styleBtn,
+											children,
+										}) => {
+
+	return (
+		<button
+			className={[styles.button, btnClasses && styles[btnClasses]].join(' ')}
+			disabled={disabled}
+			type={type}
+			onClick={onClick}
+			style={styleBtn}
+		>
+			{name}
+			{children}
+		</button>
+	);
 };
 
 export default Button;
