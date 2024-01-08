c8dddbf332d8aa1cd7d2b81cc7df3c701e6d2df7
Viktor Filippov <jackal3166@gmail.com>
2024-01-08 21:59:20 +0200

    Add args to Tail stories

--- src/components/Tail/Tail.stories.tsx
+++ src/components/Tail/Tail.stories.tsx
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import Tail from './Tail';
-import img1 from '../../assets/card-1.webp';
 
 const meta: Meta<typeof Tail> = {
 	component: Tail,
@@ -13,13 +12,20 @@ type Story = StoryObj<typeof Tail>;
 
 export const DefaultTail: Story = {
 	args: {
-		img: img1,
-		ready: true,
-		name: 'Мухтар',
-		age: '2 роки',
-		gender: 'Хлопчик',
-		description:
-			'Мухтар потрапив до нас у дуже поганому стані: знесилений, лисий та весь у ранах. Ми довго боролися за життя цього красеня і все вийшло. Тепер Мухтар справжній хвостатий захисник, а ще дуже розумний. Наші діти обожнюють цього пухнастого друга і мріють щоб Мухтар опинився у чудесному домі поруч з люблячими господарями.',
-		size: 'Великий',
+		"id": 1,
+		"name": "Бренда",
+		"ready_for_adoption": false,
+		"gender": "дівчинка",
+		"age": "4 роки",
+		"sterilization": true,
+		"vaccination_parasite_treatment": true,
+		"size": "середній",
+		"description": " Бренду знають усі. Це хвостатий символ Міста Добра. Ідеальнішу собаку годі шукати. Дружелюбна та вірна. Бренда каністерапевтка і добре відчуває, коли когось болить душа. Зцілювати дітей та дорослих – місія з якою наша чотирилапа подруга справляється на краще всіх. Любимо всім серцем",
+		"photo": {
+			"id": "4_za0fb262c29a79a118dc20518_f1153db8b5a474abd_d20240104_m143100_c005_v0501013_t0033_u01704378660619",
+			"name": "Бренда.webp",
+			"url": "https://image-bucket-234324234.s3.us-east-005.backblazeb2.com/Бренда.webp",
+			"category": "image"
+		}
 	},
 };
