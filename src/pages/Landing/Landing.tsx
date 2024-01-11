2c37d6c38aa0e04184d0af82a2074de15f906aa8
Hanna <hanna.horbova@gmail.com>
2024-01-11 19:22:31 +0200

    feat: add scrollOnTop helper function to 'Home page' page

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -3,8 +3,17 @@ import News from '../../components/News/News';
 import Support from '../../components/Support/Support';
 import Tails from '../../components/Tails/Tails';
 import Video from '../../components/Video/Video';
+import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { scrollOnTop } from '../../services/scrollTo';
 
 const Landing = () => {
+	const location = useLocation();
+
+	useEffect(() => {
+		location.pathname === '/' ? scrollOnTop() : null;
+	}, [location]);
+
 	return (
 		<main>
 			<Hero />
