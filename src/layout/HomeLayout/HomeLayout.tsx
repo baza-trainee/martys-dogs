4a279bb62fbddd0bd10ba87f082aa7744330e779
Viktor Filippov <jackal3166@gmail.com>
2024-01-19 04:43:29 +0200

    Comment Modal

--- src/layout/HomeLayout/HomeLayout.tsx
+++ src/layout/HomeLayout/HomeLayout.tsx
@@ -1,33 +1,42 @@
-import Modal from '../../components/Modal/Modal';
-// import ThanksModal from '../../components/Modal/ThanksModal';
+// import AdoptionModal from '../../components/Modal/AdoptionModal';
+
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import { Outlet } from 'react-router-dom';
+
+// import Modal from '../../components/Modal/Modal';
+
+// import { useModalContext } from '../../context/useGlobalContext';
+
+// import ThanksModal from '../../components/Modal/ThanksModal';
+
+
+
 // import { useContext } from 'react';
 // import { ModalContext } from '../../context/ModalContext';
-import { useModalContext } from '../../context/useGlobalContext';
-import AdoptionModal from '../../components/Modal/AdoptionModal';
+
+
 
 const HomeLayout = () => {
 	// const modalContext = useModalContext();
 	// const isModalOpen = modalContext?.isModalOpen || false;
 	// const openModal = modalContext?.openModal || (() => {});
 	// const closeModal = modalContext?.closeModal || (() => {});
-	const { isModalOpen, openModal, closeModal } = useModalContext();
-	console.log(isModalOpen);
+	// const { isModalOpen, openModal, closeModal } = useModalContext();
+	// console.log(isModalOpen);
 	return (
 		<>
 			<Header />
 			<Outlet />
 			<Footer />
-			<Modal
+			{/* <Modal
 				isModal={isModalOpen}
 				openModal={openModal}
 				closeModal={closeModal}
 			>
-				{/* <ThanksModal /> */}
+				<ThanksModal />
 				<AdoptionModal />
-			</Modal>
+			</Modal> */}
 		</>
 	);
 };
