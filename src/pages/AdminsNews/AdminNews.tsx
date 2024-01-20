102f739bb18c7cf67cc6935880b76b834bb69c1a
MarharytaBoichenko <boichenkomv@gmail.com>
2024-01-20 21:05:13 +0200

    add  base structure for adminnews page

--- src/pages/AdminsNews/AdminNews.tsx
+++ src/pages/AdminsNews/AdminNews.tsx
@@ -1,5 +1,33 @@
-const AdminNews = () => {
-	return <div>AdminNews</div>;
+import { Link } from 'react-router-dom';
+import { FaRegPlusSquare } from 'react-icons/fa';
+import styles from './AdminNews.module.scss';
+import { news } from '../../components/News/data';
+import AdminNewsItem, { NewsItemProps } from '../AdminsNews/AdminNewsItem';
+
+const AdminNews: React.FC = () => {
+	return (
+		<div className={styles.container}>
+			<div className={styles.header}>
+				<h2 className={styles.title}>Новини</h2>
+			</div>
+
+			<div className={styles.buttonsWrapper}>
+				<Link to='/admin/news_add'>
+					<button className={styles.addButton}>
+						<FaRegPlusSquare />
+						Додати новину
+					</button>
+				</Link>
+			</div>
+			<div className={styles.logoContainer}>
+				<ul className={styles.news}>
+					{news.map((item: NewsItemProps) => (
+						<AdminNewsItem key={item.id} {...item} />
+					))}
+				</ul>
+			</div>
+		</div>
+	);
 };
 
 export default AdminNews;
---
+++ src/pages/AdminsNews/AdminNewsItem.tsx
@@ -0,0 +1,75 @@
+import { FaTrash, FaEdit } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import styles from './AdminNewsItem.module.scss';
+
+export interface NewsItemProps {
+	id: number;
+	title: string;
+	post_at: string;
+	// update_at: string;
+	sub_text: string;
+	// url: string;
+	photo: {
+		// id: string;
+		// name: string;
+		url: string;
+		// category: string;
+	};
+}
+
+const NewsItem: React.FC<NewsItemProps> = ({
+	// id,
+	title,
+	post_at,
+	// update_at,
+	sub_text,
+	photo,
+	// url,
+}) => {
+	const months: { [key: string]: string } = {
+		січень: 'січня',
+		лютий: 'лютого',
+		березень: 'березня',
+		квітень: 'квітня',
+		травень: 'травня',
+		червень: 'червня',
+		липень: 'липня',
+		серпень: 'серпня',
+		вересень: 'вересня',
+		жовтень: 'жовтня',
+		листопад: 'листопада',
+		грудень: 'грудня',
+	};
+
+	const getDateName = (data: string) => {
+		const date = new Date(data);
+		const monthFromDate = date.toLocaleString('uk-UA', { month: 'long' });
+		const changedMonth = months[monthFromDate];
+		const stringDate = `${date.getDate()} ${changedMonth} ${date.getFullYear()}`;
+		return stringDate;
+	};
+
+	return (
+		<li className={styles.item}>
+			<div className={styles.thumb}>
+				<img src={photo.url} alt='photo' className={styles.photo} />
+			</div>
+			<div className={styles.info}>
+				<h3 className={styles.title}>{title}</h3>
+				<p className={styles.date}>{getDateName(post_at)}</p>
+				<p className={styles.text}>{sub_text}</p>
+			</div>
+			{/* <a href={url} target='blank' rel='noopener noreferrer'></a> */}
+
+			{/* move  this  div with actions  to  separate component?  */}
+			<div className={styles.logoActions}>
+				<Link to='/admin/news_edit' className={styles.link}>
+					<FaEdit className={styles.editIcon} />
+				</Link>
+				<FaTrash className={styles.deleteIcon} />
+			</div>
+		</li>
+	);
+};
+
+export default NewsItem;
