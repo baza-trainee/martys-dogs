cfbbd77555d4e49296c84e4d6b18985259d63c4a
Hanna <hanna.horbova@gmail.com>
2024-01-20 21:38:16 +0200

    fix: delete unnecessary variable

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,7 +6,7 @@ import { useTranslation } from 'react-i18next';
 
 
 const Hero = () => {
-	const { t, i18n } = useTranslation();
+	const { t } = useTranslation();
 
 	return (
 		<section className={styles.hero}>
