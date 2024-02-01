ad116e430d591af6f397264bb0f7af83b8ec70dc
MarharytaBoichenko <boichenkomv@gmail.com>
2024-02-01 22:11:37 +0200

    add  styles to  addNews form

--- src/pages/AdminsNews/AddNews.tsx
+++ src/pages/AdminsNews/AddNews.tsx
@@ -1,4 +1,6 @@
 import { FormEvent, useState, ChangeEvent } from 'react';
+// import { useForm } from 'react-hook-form';
+import { FaUpload } from 'react-icons/fa';
 import styles from './AddNews.module.scss';
 
 const AddNews: React.FC = () => {
@@ -13,10 +15,10 @@ const AddNews: React.FC = () => {
 		setTitle(evt.target.value);
 	};
 
-	const onTextChange = (evt: ChangeEvent<HTMLInputElement>) => {
-		console.log(evt.target.value);
-		setText(evt.target.value);
-	};
+	// const onTextChange = (evt: ChangeEvent<HTMLInputElement>) => {
+	// 	console.log(evt.target.value);
+	// 	setText(evt.target.value);
+	// };
 
 	const onLinkChange = (evt: ChangeEvent<HTMLInputElement>) => {
 		console.log(evt.target.value);
@@ -44,13 +46,16 @@ const AddNews: React.FC = () => {
 	};
 
 	return (
-		<div>
+		<div className={styles.container}>
+			<h3 className={styles.title}>
+				Для додавання новини необхідно заповнити всі поля
+			</h3>
 			<form className={styles.form} onSubmit={onSubmitHandler}>
-				<div>
-					<label htmlFor='image' className={styles.label}>
-						<span className={styles.text}>News photo</span>
+				<div className={styles.wrapper}>
+					<label htmlFor='image' className={styles.image}>
 						<input
-							className={styles.input}
+							// className={styles.image}
+							required
 							type='file'
 							id='image'
 							placeholder='Upload  news photo'
@@ -58,46 +63,21 @@ const AddNews: React.FC = () => {
 							// value={image}
 							onChange={onImageChange}
 						/>
+						<FaUpload className={styles.icon} />
+						<span className={styles.text}>
+							Завантажте зображення
+						</span>
 					</label>
-					<p className={styles.error}>{'errorMessage'}</p>
+					{/* <p className={styles.error}>{'errorMessage'}</p> */}
 				</div>
-				<div>
-					<label htmlFor='title' className={styles.label}>
-						<span className={styles.text}>News title</span>
-						<input
-							className={styles.input}
-							type='text'
-							id='title'
-							placeholder='Enter news title'
-							// value='title'
-							value={title}
-							onChange={onTitleChange}
-						/>
-					</label>
-					<p className={styles.error}>{'errorMessage'}</p>
-				</div>
-				<input type='text' />
-				<div>
-					<label htmlFor='text' className={styles.label}>
-						<span className={styles.text}>News text</span>
-						<input
-							className={styles.input}
-							type='text'
-							id='text'
-							placeholder='Enter news text'
-							value={text}
-							onChange={onTextChange}
-						/>
-					</label>
-					<p className={styles.error}>{'errorMessage'}</p>
-				</div>
-				<div>
+				<div className={styles.wrapper}>
 					<label htmlFor='url' className={styles.label}>
 						<span className={styles.text}>
-							News link in facebook
+							Посилання на новину в facebook
 						</span>
 						<input
 							className={styles.input}
+							required
 							type='text'
 							id='url'
 							placeholder='Enter news link in facebook'
@@ -105,7 +85,94 @@ const AddNews: React.FC = () => {
 							onChange={onLinkChange}
 						/>
 					</label>
-					<p className={styles.error}>{'errorMessage'}</p>
+					{/* <p className={styles.error}>{'errorMessage'}</p> */}
+				</div>
+				<div className={styles.group}>
+					<div className={styles.wrapper}>
+						<label htmlFor='title' className={styles.label}>
+							<span className={styles.text}>
+								Заголовок новини
+							</span>
+							<input
+								className={styles.input}
+								required
+								type='text'
+								id='title'
+								placeholder='Enter news title'
+								// value='title'
+								value={title}
+								onChange={onTitleChange}
+							/>
+						</label>
+						{/* <p className={styles.error}>{'errorMessage'}</p> */}
+					</div>
+					<div className={styles.wrapper}>
+						<label htmlFor='title' className={styles.label}>
+							<span className={styles.text}>News title</span>
+							<input
+								className={styles.input}
+								required
+								type='text'
+								id='title'
+								placeholder='Enter news title'
+								// value='title'
+								value={title}
+								onChange={onTitleChange}
+							/>
+						</label>
+						{/* <p className={styles.error}>{'errorMessage'}</p> */}
+					</div>
+				</div>
+				<div className={styles.group}>
+					<div className={styles.wrapper}>
+						<label htmlFor='text' className={styles.label}>
+							<span className={styles.text}>Текст новини</span>
+							<textarea
+								className={styles.textarea}
+								maxLength={250}
+								rows={6}
+								required
+								// type='text'
+								id='text'
+								placeholder='Enter news text'
+								value={text}
+								// onChange={onTextChange}
+							/>
+						</label>
+						{/* <p className={styles.error}>{'errorMessage'}</p> */}
+					</div>
+
+					<div className={styles.wrapper}>
+						<label htmlFor='text' className={styles.label}>
+							<span className={styles.text}>News text</span>
+							<textarea
+								className={styles.textarea}
+								maxLength={250}
+								rows={6}
+								required
+								// type='text'
+								id='text'
+								placeholder='Enter news text'
+								value={text}
+								// onChange={onTextChange}
+							/>
+							{/* <input
+								className={styles.input}
+								type='text'
+								id='text'
+								placeholder='Enter news text'
+								value={text}
+								onChange={onTextChange}
+							/> */}
+						</label>
+						{/* <p className={styles.error}>{'errorMessage'}</p> */}
+					</div>
+				</div>
+				<div className={styles.button_wrapper}>
+					<button className={styles.addButton} type='submit'>
+						Додати
+					</button>
+					<button className={styles.addButton}>Відмінити</button>
 				</div>
 			</form>
 		</div>
@@ -113,3 +180,187 @@ const AddNews: React.FC = () => {
 };
 
 export default AddNews;
+
+// const AddNews: React.FC = () => {
+// 	const [title, setTitle] = useState('');
+// 	const [text, setText] = useState('');
+// 	const [, setImage] = useState<File | null>(null);
+// 	// const [date, setDate] = useState('');
+// 	const [link, setLink] = useState('');
+
+// 	const onTitleChange = (evt: ChangeEvent<HTMLInputElement>) => {
+// 		console.log(evt.target.value);
+// 		setTitle(evt.target.value);
+// 	};
+
+// 	// const onTextChange = (evt: ChangeEvent<HTMLInputElement>) => {
+// 	// 	console.log(evt.target.value);
+// 	// 	setText(evt.target.value);
+// 	// };
+
+// 	const onLinkChange = (evt: ChangeEvent<HTMLInputElement>) => {
+// 		console.log(evt.target.value);
+// 		setLink(evt.target.value);
+// 	};
+
+// 	const onImageChange = (evt: ChangeEvent<HTMLInputElement>) => {
+// 		// console.log(evt.target.files[0]);
+// 		const uploadedImage = evt.target.files?.[0];
+// 		if (uploadedImage) setImage(uploadedImage);
+// 	};
+
+// 	const resetForm = () => {
+// 		setImage(null);
+// 		setLink('');
+// 		setText('');
+// 		setTitle('');
+// 	};
+
+// 	const onSubmitHandler = (evt: FormEvent) => {
+// 		evt.preventDefault();
+// 		// 	validate data
+// 		// 	send data to back
+// 		resetForm();
+// 	};
+
+// 	return (
+// 		<div className={styles.container}>
+// 			<h3 className={styles.title}>
+// 				Для додавання новини необхідно заповнити всі поля
+// 			</h3>
+// 			<form className={styles.form} onSubmit={onSubmitHandler}>
+// 				<div className={styles.wrapper}>
+// 					<label htmlFor='image' className={styles.image}>
+// 						<input
+// 							// className={styles.image}
+// 							required
+// 							type='file'
+// 							id='image'
+// 							placeholder='Upload  news photo'
+// 							accept='image/*'
+// 							// value={image}
+// 							onChange={onImageChange}
+// 						/>
+// 						<FaUpload className={styles.icon} />
+// 						<span className={styles.text}>
+// 							Завантажте зображення
+// 						</span>
+// 					</label>
+// 					{/* <p className={styles.error}>{'errorMessage'}</p> */}
+// 				</div>
+// 				<div className={styles.wrapper}>
+// 					<label htmlFor='url' className={styles.label}>
+// 						<span className={styles.text}>
+// 							Посилання на новину в facebook
+// 						</span>
+// 						<input
+// 							className={styles.input}
+// 							required
+// 							type='text'
+// 							id='url'
+// 							placeholder='Enter news link in facebook'
+// 							value={link}
+// 							onChange={onLinkChange}
+// 						/>
+// 					</label>
+// 					{/* <p className={styles.error}>{'errorMessage'}</p> */}
+// 				</div>
+// 				<div className={styles.group}>
+// 					<div className={styles.wrapper}>
+// 						<label htmlFor='title' className={styles.label}>
+// 							<span className={styles.text}>
+// 								Заголовок новини
+// 							</span>
+// 							<input
+// 								className={styles.input}
+// 								required
+// 								type='text'
+// 								id='title'
+// 								placeholder='Enter news title'
+// 								// value='title'
+// 								value={title}
+// 								onChange={onTitleChange}
+// 							/>
+// 						</label>
+// 						{/* <p className={styles.error}>{'errorMessage'}</p> */}
+// 					</div>
+// 					<div className={styles.wrapper}>
+// 						<label htmlFor='title' className={styles.label}>
+// 							<span className={styles.text}>News title</span>
+// 							<input
+// 								className={styles.input}
+// 								required
+// 								type='text'
+// 								id='title'
+// 								placeholder='Enter news title'
+// 								// value='title'
+// 								value={title}
+// 								onChange={onTitleChange}
+// 							/>
+// 						</label>
+// 						{/* <p className={styles.error}>{'errorMessage'}</p> */}
+// 					</div>
+// 				</div>
+// 				<div className={styles.group}>
+// 					<div className={styles.wrapper}>
+// 						<label htmlFor='text' className={styles.label}>
+// 							<span className={styles.text}>Текст новини</span>
+// 							<textarea
+// 								className={styles.textarea}
+// 								maxLength={250}
+// 								rows={6}
+// 								required
+// 								// type='text'
+// 								id='text'
+// 								placeholder='Enter news text'
+// 								value={text}
+// 								// onChange={onTextChange}
+// 							/>
+// 							{/* <input
+// 								className={styles.input}
+// 								type='text'
+// 								id='text'
+// 								placeholder='Enter news text'
+// 								value={text}
+// 								onChange={onTextChange}
+// 							/> */}
+// 						</label>
+// 						{/* <p className={styles.error}>{'errorMessage'}</p> */}
+// 					</div>
+
+// 					<div className={styles.wrapper}>
+// 						<label htmlFor='text' className={styles.label}>
+// 							<span className={styles.text}>News text</span>
+// 							<textarea
+// 								className={styles.textarea}
+// 								maxLength={250}
+// 								rows={6}
+// 								required
+// 								// type='text'
+// 								id='text'
+// 								placeholder='Enter news text'
+// 								value={text}
+// 								// onChange={onTextChange}
+// 							/>
+// 							{/* <input
+// 								className={styles.input}
+// 								type='text'
+// 								id='text'
+// 								placeholder='Enter news text'
+// 								value={text}
+// 								onChange={onTextChange}
+// 							/> */}
+// 						</label>
+// 						{/* <p className={styles.error}>{'errorMessage'}</p> */}
+// 					</div>
+// 				</div>
+// 				<div className={styles.button_wrapper}>
+// 					<button className={styles.addButton} type='submit'>
+// 						Додати
+// 					</button>
+// 					<button className={styles.addButton}>Відмінити</button>
+// 				</div>
+// 			</form>
+// 		</div>
+// 	);
+// };
--- src/pages/AdminsNews/AdminNews.tsx
+++ src/pages/AdminsNews/AdminNews.tsx
@@ -1,9 +1,73 @@
+import { useQuery } from '@tanstack/react-query';
 import styles from './AdminNews.module.scss';
 import { news } from '../../components/News/data';
 import AdminNewsItem, { NewsItemProps } from '../AdminsNews/AdminNewsItem';
 import AddButton from '../../layout/AddButton/AddButton';
 
+interface Photo {
+	id: string;
+	name: string;
+	url: string;
+	category: string;
+}
+interface NewsItem {
+	id: number;
+	title: string;
+	post_at: string;
+	update_at: string;
+	sub_text: string;
+	url: string;
+	photo: Photo;
+}
+
+export interface NewsData {
+	news: NewsItem[];
+}
+
+const NEWS = 'https://matys-dogs2.onrender.com/news';
+
+const fetchNews = async () => {
+	try {
+		const response = await fetch(NEWS);
+
+		if (!response.ok) {
+			throw new Error('Data loading error');
+		}
+
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.error('Error while loading data:', error);
+		throw error;
+	}
+};
+
 const AdminNews: React.FC = () => {
+	const data = useQuery<NewsData>({
+		queryKey: ['news'],
+		queryFn: fetchNews,
+		refetchInterval: 600000,
+	});
+
+	const { data: news, isPending, isError, error } = data;
+
+	console.log(news?.news);
+	if (isPending) {
+		return (
+			<div className={styles.container}>
+				<div className={styles.loading}></div>
+			</div>
+		);
+	}
+
+	if (isError) {
+		return (
+			<div className={styles.container}>
+				<div className={styles.alert}>{error.message}</div>
+			</div>
+		);
+	}
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.header}>
@@ -11,7 +75,7 @@ const AdminNews: React.FC = () => {
 			</div>
 			<AddButton path='news_add' text='новину' />
 			<ul className={styles.news}>
-				{news.map((item: NewsItemProps) => (
+				{news?.news.map((item: NewsItemProps) => (
 					<AdminNewsItem key={item.id} {...item} />
 				))}
 			</ul>
