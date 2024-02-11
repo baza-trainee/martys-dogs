59bba7de3a45ca61663c75dca1ffe0956b96434e
Svitlana T <kirilsveta@gmail.com>
2024-02-11 04:30:19 -0800

    Add some functionality for partners component, add background prop for Loader  and changed images in video on the homepage

--- src/pages/AdminPartners/AddPartner.tsx
+++ src/pages/AdminPartners/AddPartner.tsx
@@ -1,17 +1,41 @@
 import React from 'react';
 import ChangePartners from './ChangePartners';
+import { useMutation, useQueryClient} from '@tanstack/react-query';
+import { useNavigate} from 'react-router-dom';
+import { PartnersStateType } from './ChangePartners';
+import { requestAdminPage } from '../../services/adminPartners';
+import { createFormData } from './ChangePartners';
+import { transformFormData } from './ChangePartners';
+
 
 const AddPartner: React.FC = () => {
+	const navigate = useNavigate();
+
+  const addPartnerMutation = useMutation({
+    mutationFn:(data: PartnersStateType) => requestAdminPage('POST', '/partners', data)
+});
+
+  const handleSubmit = (state: PartnersStateType) => {
+    const formData = createFormData(state);
+
+    console.log('Transformed Form Data:', transformFormData(formData));
+
+    addPartnerMutation.mutate(transformFormData(formData), {
+      onSuccess: () => {
+        console.log('Add partner successful!');
+        navigate('/admin/partners');
+      },
+      onError: () => {
+        console.error('Add partner failed!');
+      },
+    });
+  };
+
   return (
     <ChangePartners
       title="Додати партнера"
-      labelName="Назва"
-      labelImage="Логотип"
-      labelWeb='Сайт партнера'
-      placeholderName="Введіть назву"
-      placeholderWeb="Додайте посилання"
       submitButtonName="Додати"
-      cancelButtonName="Скасувати"
+      onSubmit={handleSubmit}
     />
   );
 };
--- src/pages/AdminPartners/ChangePartners.tsx
+++ src/pages/AdminPartners/ChangePartners.tsx
@@ -1,108 +1,208 @@
-import React, { useState } from 'react';
-import styles from './ChangePartners.module.scss';
+import React, {useState} from 'react';
+import { useForm, SubmitHandler} from 'react-hook-form';
 import { FaUpload } from 'react-icons/fa';
+import {Loader, ErrorAlert} from '../../components/CommonUI/LoaderAndError/LoaderAndError';
+import styles from './ChangePartners.module.scss';
 
+export interface PartnersStateType {
+	inputName: string;
+	inputWeb: string;
+	image: File | null;
+}
 
 interface Props {
-  title: string;
-  labelName: string;
-  labelImage: string;
-  labelWeb:string;
-  placeholderName: string;
-  placeholderWeb: string;
-  submitButtonName: string;
-  cancelButtonName: string;
+	title: string;
+	submitButtonName: string;
+	onSubmit: (state: PartnersStateType) => void;
 }
 
-interface State {
-  inputName: string;
-  inputWeb: string;
-  image: File | null;
-}
+export const createFormData = (data: PartnersStateType): FormData => {
+	const formData = new FormData();
+	formData.append('inputName', data.inputName);
+	formData.append('inputWeb', data.inputWeb);
+	if (data.image !== null) {
+		formData.append('image', data.image as Blob);
+	  }
+	return formData;
+  };
+
+export const transformFormData = (
+	data: FormData,
+): { inputName: string; inputWeb: string; image: File | null } => {
+	return {
+		inputName: data.get('inputName') as string,
+		inputWeb: data.get('inputWeb') as string,
+		image: data.get('image') instanceof File ? data.get('image') as File : null,
+	};
+};
 
 const ChangePartners: React.FC<Props> = ({
-  title,
-  labelName,
-  labelImage,
-  labelWeb,
-  placeholderName,
-  placeholderWeb,
-  submitButtonName,
-  cancelButtonName,
+	title,
+	submitButtonName,
+	onSubmit,
 }) => {
-  const initialState: State = {
-    inputName: '',
-    inputWeb: '',
-    image: null,
-  };
 
-  const [state, setState] = useState<State>(initialState);
+	const {
+		register,
+		handleSubmit,
+		setValue,
+		watch,
+		formState: { errors, isSubmitted },
+		getValues,
+		reset
+	} = useForm<PartnersStateType>({
+		defaultValues: {
+			inputName: '',
+			inputWeb: '',
+			image: null,
+		},
+	});
+	const [isSubmitting, setIsSubmitting] = useState(false);
+  const [errorAlert, setErrorAlert] = useState(false);
+	const watchImage = watch('image');
 
-  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (file) {
-      setState((prev) => ({ ...prev, image: file }));
-    }
-  };
+	const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const file = e.target.files?.[0];
+		setValue('image', file || null);
+		console.log('Form State:', getValues());
+		console.log('watchImage:', file);
+	};
 
-  const handleSubmit = () => {
-    //дописати функцію 
-    console.log('Submit clicked!');
-  };
+	const handleCancel = () => {
+		setValue('inputName', '');
+		setValue('inputWeb', '');
+		setValue('image', null);
+		console.log('Cancel clicked!');
+	};
 
-  const handleCancel = () => {
-   //дописати функцію
-    console.log('Cancel clicked!');
-  };
+	
+	const submitHandler: SubmitHandler<PartnersStateType> =  (data) => {
+		console.log('Form data submitted:', data);
+		setIsSubmitting(true);
 
-  return (
-    <div className={styles.container}>
-      <h2>{title}</h2>
+		try {
+		   onSubmit(data);
 
-      <div className={styles.inputRow}>
-        <div className={styles.inputContainer}>
-          <label htmlFor="name">{labelName}:</label>
-          <input
-            type="text"
-            id="name"
-            value={state.inputName}
-            onChange={(e) => setState((prev) => ({ ...prev, inputName: e.target.value }))}
-            placeholder={placeholderName}
-          />
-        </div>
+		  reset();
+		  setIsSubmitting(false);
+		  console.log('Submit clicked!');
+		} catch (error) {
 
-        <div className={styles.inputContainer}>
-          <label htmlFor="imageInput">{labelImage}:</label>
-          <label className={styles.uploadLabel} htmlFor="imageInput">
-            <FaUpload className={styles.uploadIcon} />
-            {state.image ? state.image.name : 'Завантажте зображення'}
-          </label>
-          <input
-            type="file"
-            id="imageInput"
-            onChange={handleImageChange}
-            accept="image/*"
-          />
-        </div>
+		  setIsSubmitting(false);
+		  console.error('Error during form submission:', error);
+		  setErrorAlert(true);
+		}
+	  };
+	return (
+		<div className={styles.container}>
+			<h2>{title}</h2>
+			
+			<form onSubmit={handleSubmit(submitHandler)} className={styles.form}>
+				<div className={styles.inputRow}>
+					<div className={styles.inputContainer}>
+						<label htmlFor='name'>Назва:</label>
+						<input
+							type='text'
+							id='name'
+							{...register('inputName', {
+								required: "Назва обов'язкова",
+								maxLength: {
+									value: 50,
+									message:
+										'Назва повинна бути менше або дорівнювати 50 символам',
+								},
+							})}
+							placeholder='Введіть назву'
+						/>
+						{errors.inputName && (
+							<div className={styles.validationError}>
+								{errors.inputName.message}
+							</div>
+						)}
+					</div>
 
-        <div className={styles.inputContainer}>
-          <label htmlFor="website">{labelWeb}:</label>
-          <input
-            type="text"
-            id="website"
-            value={state.inputWeb}
-            onChange={(e) => setState((prev) => ({ ...prev, inputWeb: e.target.value }))}
-            placeholder={placeholderWeb}
-          />
-        </div>
-      </div>
+					<div className={styles.inputContainer}>
+						<label htmlFor='imageInput'>
+							Логотип:
+							<span className={styles.uploadLabel}>
+								<FaUpload className={styles.uploadIcon} />
+								{watchImage?.name || 'Завантажте зображення'}
+							</span>
+						</label>
+						<input
+							type='file'
+							id='imageInput'
+							{...register('image', {
+								required: isSubmitted ?  false:'Зображення не обране',
+								validate: {
+									validImageFormat: (value: File | null) => {
+										if (!value) return true;
+										const supportedImageFormats = [
+											'image/jpeg',
+											'image/png',
+											'image/webp',
+										];
+										return (
+											supportedImageFormats.includes(
+												value.type,
+											) ||
+											'Виберіть дійсний файл зображення (JPEG, PNG або WebP)'
+										);
+									},
+									validImageSize: (value: File | null) => {
+										if (!value) return true;
+										const maxSize = 5 * 1024 * 1024; // 5MB
+										return (
+											value.size <= maxSize ||
+											`Розмір файлу повинен бути менше або рівний ${
+												maxSize / (1024 * 1024)
+											} MB`
+										);
+									},
+								},
+							})}
+							onChange={handleImageChange}
+							accept='image/*'
+						/>
+						{errors.image && (
+							<div className={styles.validationError}>
+								{errors.image.message}
+							</div>
+						)}
+					</div>
 
-      <div className={styles.buttonRow}>
-        <button onClick={handleSubmit}>{submitButtonName}</button>
-        <button onClick={handleCancel}>{cancelButtonName}</button>
-      </div>
-    </div>
-  );
+					<div className={styles.inputContainer}>
+						<label htmlFor='website'>Сайт партнера:</label>
+						<input
+							type='text'
+							id='website'
+							{...register('inputWeb', {
+								required: "Веб-сайт обов'язковий",
+								pattern: {
+									value: /^(https?:\/\/)?([\w-]+\.)+[\w]{2,}(\/[\w-]*)*$/,
+									message: 'Введіть дійсний URL веб-сайту',
+								},
+							})}
+							placeholder='Додайте посилання'
+						/>
+						{errors.inputWeb && (
+							<div className={styles.validationError}>
+								{errors.inputWeb.message}
+							</div>
+						)}
+					</div>
+				</div>
+				{isSubmitting && <Loader backgroundColor='#EEEEEE'/>}
+				{errorAlert && <ErrorAlert errorMessage="Submission failed." backgroundColor='#EEEEEE'/>}
+				<div className={styles.buttonRow}>
+					<button type='submit'>{submitButtonName}</button>
+					<button type='button' onClick={handleCancel}>
+						Скасувати
+					</button>
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default ChangePartners;
