8b44706952e61b344021ac6a907eff83f9544999
MarharytaBoichenko <boichenkomv@gmail.com>
2024-02-15 16:52:55 +0200

    resolve  conflict in  App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ import Admins from './pages/Admins/Admins';
 import { AuthProvider } from './context/AuthContext';
 import Contacts from './pages/Contacts/Contacts';
 import AddEditNews from './pages/AdminsNews/AddEditNews';
-import EditPartner from './pages/AdminPartners/EditPartner';
 import Error from './pages/Error/Error';
 import HomeLayout from './layout/HomeLayout/HomeLayout';
 import Landing from './pages/Landing/Landing';
@@ -24,107 +23,103 @@ import OurTails from './pages/OurTails/OurTails';
 import Register from './pages/Register/Register';
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 5,
-    },
-  },
+	defaultOptions: {
+		queries: {
+			staleTime: 1000 * 60 * 5,
+		},
+	},
 });
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomeLayout />,
-    errorElement: <Error />,
-    children: [
-      {
-        index: true,
-        element: <Landing />,
-      },
-      {
-        path: 'about',
-        element: <About />,
-      },
-      {
-        path: 'contacts',
-        element: <Contacts />,
-      },
-      {
-        path: 'tails',
-        element: <OurTails />,
-      },
-    ],
-  },
-  {
-    path: 'login',
-    element: <Login />,
-  },
-  {
-    path: 'register',
-    element: <Register />,
-  },
-  {
-    path: 'admin',
-    element: <AdminHomeLayout />,
-    children: [
-      {
-        index: true,
-        element: <AdminDogs />,
-      },
-      {
-        path: 'partners',
-        element: <AdminPartners />,
-      },
-      {
-        path: 'partner_add',
-        element: <AddPartner />,
-      },
-      {
-        path: 'partner_edit',
-        element: <EditPartner />,
-      },
-      {
-        path: 'news',
-        element: <AdminNews />,
-      },
-      {
-        path: 'news_add',
-        element: <AddEditNews />,
-      },
-      {
-        path: 'news_edit/:newsId',
-        element: <AddEditNews />,
-      },
-      {
-        path: 'statistics',
-        element: <AdminStatistics />,
-      },
-      {
-        path: 'photos',
-        element: <AdminPhotos />,
-      },
-      {
-        path: 'sms',
-        element: <AdminForm />,
-      },
-      {
-        path: 'pass',
-        element: <Admins />,
-      },
-    ],
-  },
+	{
+		path: '/',
+		element: <HomeLayout />,
+		errorElement: <Error />,
+		children: [
+			{
+				index: true,
+				element: <Landing />,
+			},
+			{
+				path: 'about',
+				element: <About />,
+			},
+			{
+				path: 'contacts',
+				element: <Contacts />,
+			},
+			{
+				path: 'tails',
+				element: <OurTails />,
+			},
+		],
+	},
+	{
+		path: 'login',
+		element: <Login />,
+	},
+	{
+		path: 'register',
+		element: <Register />,
+	},
+	{
+		path: 'admin',
+		element: <AdminHomeLayout />,
+		children: [
+			{
+				index: true,
+				element: <AdminDogs />,
+			},
+			{
+				path: 'partners',
+				element: <AdminPartners />,
+			},
+			{
+				path: 'partner_add',
+				element: <AddPartner />,
+			},
+			{
+				path: 'news',
+				element: <AdminNews />,
+			},
+			{
+				path: 'news_add',
+				element: <AddEditNews />,
+			},
+			{
+				path: 'news_edit/:newsId',
+				element: <AddEditNews />,
+			},
+			{
+				path: 'statistics',
+				element: <AdminStatistics />,
+			},
+			{
+				path: 'photos',
+				element: <AdminPhotos />,
+			},
+			{
+				path: 'sms',
+				element: <AdminForm />,
+			},
+			{
+				path: 'pass',
+				element: <Admins />,
+			},
+		],
+	},
 ]);
 
 const App = () => {
-  return (
-    <AuthProvider>
-      <ModalProvider>
-        <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router} />
-        </QueryClientProvider>
-      </ModalProvider>
-    </AuthProvider>
-  );
+	return (
+		<AuthProvider>
+			<ModalProvider>
+				<QueryClientProvider client={queryClient}>
+					<RouterProvider router={router} />
+				</QueryClientProvider>
+			</ModalProvider>
+		</AuthProvider>
+	);
 };
 
 export default App;
