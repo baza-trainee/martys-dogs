184a0d63f5c550ebba769ae2be86afd2cbbd31ab
Viktor Filippov <jackal3166@gmail.com>
2024-02-19 18:53:11 +0200

    Add registration

--- src/components/FormInput/FormInput.tsx
+++ src/components/FormInput/FormInput.tsx
@@ -3,15 +3,21 @@ import styles from './FormInput.module.scss';
 interface FormInputProps {
 	label: string;
 	id: string;
+	name: string;
+	type:'email' | 'text' | 'password';
 	value: string;
 	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+	errorMessage?: string;
 }
 
 const FormInput: React.FC<FormInputProps> = ({
 	label,
 	id,
+	name,
+	type,
 	value,
-	onChange
+	onChange,
+	errorMessage
 }) => {
 	return (
 		<div className={styles.form}>
@@ -19,12 +25,16 @@ const FormInput: React.FC<FormInputProps> = ({
 				<span className={styles.text}>{label}</span>
 			</label>
 			<input
-				type={id}
 				id={id}
+				name={name}
+				type={type}
 				value={value}
 				onChange={onChange}
 				className={styles.input}
 			/>
+			{errorMessage && ( 
+        <span className={styles.error}>{errorMessage}</span>
+      )}
 		</div>
 	);
 };
