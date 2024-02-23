dbc1d7f356d97df0098fad182927ff9fe879c854
Viktor Filippov <jackal3166@gmail.com>
2024-02-23 15:37:29 +0200

    Fix Photos.test

--- src/components/Photos/Photos.test.tsx
+++ src/components/Photos/Photos.test.tsx
@@ -1,41 +1,39 @@
+import { MemoryRouter } from 'react-router-dom';
 import {
 	QueryClient,
 	QueryClientProvider,
 	UseQueryResult,
 	useQuery,
 } from '@tanstack/react-query';
+
+import Photos from './Photos';
+import { AboutData } from '../../pages/About/About';
 import { render, screen, waitFor } from '../../utils/test-utils';
 
-import { FetchAboutResult } from '../../pages/About/About';
-import { MemoryRouter } from 'react-router-dom';
-import Photos from './Photos';
-
-export const mockAboutData: FetchAboutResult = {
-	about_data: [
-		{
-			quantity_of_animals: 100,
-			quantity_of_employees: 20,
-			quantity_of_succeeds_adoptions: 50,
-			images: [
-				{
-					id: '1',
-					name: 'Animal Shelter Exterior',
-					url: 'https://example.com/animal-shelter-exterior.jpg',
-					category: 'Exterior',
-				},
-				{
-					id: '2',
-					name: 'Happy Pets Adoption Event',
-					url: 'https://example.com/happy-pets-adoption-event.jpg',
-					category: 'Event',
-				},
-			],
-		},
-	],
-};
+export const mockAboutData: AboutData[] = [
+	{
+		quantity_of_animals: 100,
+		quantity_of_employees: 20,
+		quantity_of_succeeds_adoptions: 50,
+		images: [
+			{
+				id: '1',
+				name: 'Animal Shelter Exterior',
+				url: 'https://example.com/animal-shelter-exterior.jpg',
+				category: 'Exterior',
+			},
+			{
+				id: '2',
+				name: 'Happy Pets Adoption Event',
+				url: 'https://example.com/happy-pets-adoption-event.jpg',
+				category: 'Event',
+			},
+		],
+	},
+];
 
 const MockPhotos: React.FC = () => {
-	const mockQueryResult: UseQueryResult<FetchAboutResult, Error> = useQuery({
+	const mockQueryResult: UseQueryResult<AboutData[], Error> = useQuery({
 		queryKey: ['about'],
 		queryFn: async () => mockAboutData,
 	});
