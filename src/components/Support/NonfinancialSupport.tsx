2a996aeeb8c4ced4ab87351572bba94d01ead6d1
Yevheniia Dolzhykova <nikola.tesla0510@gmail.com>
2024-03-04 18:33:11 +0100

    fixed styles in section support

--- src/components/Support/NonfinancialSupport.tsx
+++ src/components/Support/NonfinancialSupport.tsx
@@ -17,41 +17,41 @@ const NonfinancialSupport = () => {
       <div className={style.address__container}>
       <h3 className={`${style.address__title} ${style.title}`}>{t('support.parcel')}</h3>
        
-          <dl className={style.address__box}>
+          <div className={style.address__box}>
             
             <div className={style.address__position__column}>
               <div className={style.address__item__position}>
-                <dt><BiMap className={style.icon}  /></dt>
-            <dd className={style.caption}>{t("support.city") }</dd>
+                <BiMap className={style.icon}  />
+            <p className={style.caption}>{t("support.city") }</p>
               </div>
               <div className={style.address__item__position}>
-                <dt> <BiUser className={style.icon} /></dt>
+                <BiUser className={style.icon} />
                 <div>
-              <dd className={style.caption}>{t("support.name")}</dd>
-                  <dd  className={style.description}>+380 50 243 6971</dd>
+              <p className={style.caption}>{t("support.name")}</p>
+                  <p className={style.description}>+380 50 243 6971</p>
                   </div>
               </div>
             </div>
             
             <div  className={style.address__position__row}>
               <div>
-            <dt className={style.caption}>{t('support.nova_poshta') }</dt>
-            <dd className={style.description}>{t('support.nova_poshta_point') }</dd>
-            <dd className={style.description}>{t('support.nova_poshta_street') }</dd>
+            <p className={style.caption}>{t('support.nova_poshta') }</p>
+            <p className={style.description}>{t('support.nova_poshta_point') }</p>
+            <p className={style.description}>{t('support.nova_poshta_street') }</p>
               </div>
               <img src={`${novaPost}`} alt='Логотип Нової пошти'  className={style.image } />
             </div>
             
             <div  className={style.address__position__row}>
               <div>
-            <dt className={style.caption}>{t('support.ukr_poshta') }</dt>
-            <dd className={style.description}>{t('support.ukr_poshta_point') }</dd>
-            <dd className={style.description}>{t('support.ukr_poshta_street') }</dd>
+            <p className={style.caption}>{t('support.ukr_poshta') }</p>
+            <p className={style.description}>{t('support.ukr_poshta_point') }</p>
+            <p className={style.description}>{t('support.ukr_poshta_street') }</p>
               </div>
               <img src={`${ukrPost}`} alt='Логотип Укр пошти' className={style.image } />
             </div>
             
-          </dl>
+          </div>
 </div>
     
   
